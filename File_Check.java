5d682c34461050d3702fd6c69b850ad98bb32b83
Sajen Vasuthevan <61668091+sajenvasu@users.noreply.github.com>
2021-04-04 02:31:00 -0400

    Add files via upload

--- File_Check.java
+++ File_Check.java
@@ -21,14 +21,17 @@ Features in the Program (List is specified as a Linked List)
 */
 
 import java.util.*;
+import java.io.*;
 
 public class File_Check{
 
     public static void main(String[] args){
 
-        // File F = new File();
-        
         int Run = 1;
+        String Name;
+        String Pin;
+
+        Read();
 
         while (Run == 1){
 
@@ -39,15 +42,15 @@ public class File_Check{
             System.out.println("4. Modify File Path");
             System.out.print("5. Exit\n>> ");
             String choice = Scanner.nextLine();
-            
+
             if (choice.equals("1")){
 
             }else if (choice.equals("2")){
 
             }else if (choice.equals("3")){
-                
+
             }else if (choice.equals("4")){
-                
+
             }else if (choice.equals("5")){
                 Run = 0;
             }else {
@@ -61,7 +64,48 @@ public class File_Check{
     }
 
     public static void Read(){
+
+        String Name;
+        String Pin = null;
+        Scanner Scanner = new Scanner(System.in);
+
+        File File1 = new File("File Check Program Files/pin.txt");
+        boolean EXST1 = File1.exists();
+
+        File File2 = new File("File Check Program Files/path.txt");
+        boolean EXST2 = File2.exists();
         
+        if(EXST1 == false){
+
+            System.out.print("Enter your name >> "); 
+            Name = Scanner.nextLine();
+            System.out.println();
+
+            int x = 1;
+            while(x == 1){
+                
+                System.out.print("Enter a 4-digit pin >> "); 
+                Pin = Scanner.nextLine();
+                
+                if (Pin.length() == 4){
+                    x = 0;
+                }else{
+                    System.out.println("Please enter a valid 4-digit pin!\n");
+                }
+
+            }
+
+            //Write(Name, Pin, 1);
+        }
+
+        if (EXST2 == false){
+            //Write(null, null, 2); 
+        }
+
+        System.out.println("");
+
     }
+
+
 
 }
